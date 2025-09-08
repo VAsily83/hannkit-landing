0ce01bb6f56420aaa7d33024568ff619091ea1fd
VAsily83 <Mashkov_Vasily@mail.ru>
2025-09-08 12:07:29 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
+// pages/_app.tsx
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import 'react-phone-number-input/style.css'; // ← добавили
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
